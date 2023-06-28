605fa5a4fd60168432ea56f865e5572387d2157a
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2023-06-28 16:12:32 +0900

    json format parsing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
 import xml.etree.ElementTree as ET
+import json
+import csv
 
 passcode = pd.read_csv('passcode.csv', index_col=0)
 passcodey = passcode.iloc[0][0]
@@ -12,12 +14,31 @@ PARAMS = {'serviceKey': passcodey,
           'pageNo': 1,
           'base_date': 20230628,
           'base_time': '0600',
+          'dataType': 'JSON',
           'nx': 55,
           'ny': 127}
 
 r = requests.get(url = URL, params = PARAMS)
 
 
+
+jsondata = json.loads(r.content)
+data_file = open('Gjsonoutput.csv', 'w', newline='')
+csv_writer = csv.writer(data_file)
+
+
+
+count = 0
+for data in jsondata['response']['body']['items']['item']:
+    if count == 0:
+        header = data.keys()
+        csv_writer.writerow(header)
+        count += 1
+    csv_writer.writerow(data.values())
+
+data_file.close()
+
+"""
 root = ET.fromstring(r.content)
 
 cols = ["baseDate", "baseTime", "category", "nx", "ny", "obsrValue"]
@@ -59,5 +80,5 @@ df = pd.DataFrame(rows, columns=cols)
 
 # Writing dataframe to csv
 df.to_csv('output.csv')
-
+"""
 
