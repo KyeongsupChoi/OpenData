11142ae06eb75a85f07d18984155f47a61c1b506
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2023-06-28 15:43:59 +0900

    generate output csv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import requests
-from xml.etree import ElementTree
+import xml.etree.ElementTree as ET
 
 passcode = pd.read_csv('passcode.csv', index_col=0)
 passcodey = passcode.iloc[0][0]
@@ -18,5 +18,46 @@ PARAMS = {'serviceKey': passcodey,
 r = requests.get(url = URL, params = PARAMS)
 
 
-tree = ElementTree.fromstring(r.content)
-print(r.content)
+root = ET.fromstring(r.content)
+
+cols = ["baseDate", "baseTime", "category", "nx", "ny", "obsrValue"]
+rows = []
+
+rooty = root.get('items')
+
+print(type(root))
+
+baseDate = ''
+baseTime = ''
+category = ''
+nx = ''
+ny = ''
+obsrValue = ''
+
+for i in root.iter():
+    if i.tag == 'baseDate':
+        baseDate = i.text
+    elif i.tag == 'baseTime':
+        baseTime = i.text
+    elif i.tag == 'category':
+        category = i.text
+    elif i.tag == 'nx':
+        nx = i.text
+    elif i.tag == 'ny':
+        ny = i.text
+    elif i.tag == 'obsrValue':
+        obsrValue = i.text
+
+        rows.append({"baseDate": baseDate,
+                 "baseTime": baseTime,
+                 "category": category,
+                 "nx": nx,
+                 "ny": ny,
+                 'obsrValue': obsrValue})
+
+df = pd.DataFrame(rows, columns=cols)
+
+# Writing dataframe to csv
+df.to_csv('output.csv')
+
+
