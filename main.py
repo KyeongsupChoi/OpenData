08723739dc430954dd1906e89c079b03a5c4d9d5
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2023-06-29 09:59:05 +0900

    cleaned and commented code

--- main.py
+++ main.py
@@ -4,32 +4,41 @@ import xml.etree.ElementTree as ET
 import json
 import csv
 
+# Load password from fil in gitignore
 passcode = pd.read_csv('passcode.csv', index_col=0)
 passcodey = passcode.iloc[0][0]
 
+# Ultra short Korea now cast API address
 URL = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst"
 
+# Parameters for api call
 PARAMS = {'serviceKey': passcodey,
           'numOfRows': 10,
           'pageNo': 1,
-          'base_date': 20230628,
+          'base_date': 20230629,
           'base_time': '0600',
           'dataType': 'JSON',
           'nx': 55,
           'ny': 127}
 
-r = requests.get(url = URL, params = PARAMS)
+# Get request to api address with parameters
+r = requests.get(url=URL, params=PARAMS)
 
-
-
+# Load json content
 jsondata = json.loads(r.content)
+
+# Select body content
+jsondata = jsondata['response']['body']['items']['item']
+
+# Open csv file
 data_file = open('Gjsonoutput.csv', 'w', newline='')
+
+# Initialize csv writer
 csv_writer = csv.writer(data_file)
 
-
-
+# Write to csv
 count = 0
-for data in jsondata['response']['body']['items']['item']:
+for data in jsondata:
     if count == 0:
         header = data.keys()
         csv_writer.writerow(header)
@@ -37,48 +46,4 @@ for data in jsondata['response']['body']['items']['item']:
     csv_writer.writerow(data.values())
 
 data_file.close()
-
-"""
-root = ET.fromstring(r.content)
-
-cols = ["baseDate", "baseTime", "category", "nx", "ny", "obsrValue"]
-rows = []
-
-rooty = root.get('items')
-
-print(type(root))
-
-baseDate = ''
-baseTime = ''
-category = ''
-nx = ''
-ny = ''
-obsrValue = ''
-
-for i in root.iter():
-    if i.tag == 'baseDate':
-        baseDate = i.text
-    elif i.tag == 'baseTime':
-        baseTime = i.text
-    elif i.tag == 'category':
-        category = i.text
-    elif i.tag == 'nx':
-        nx = i.text
-    elif i.tag == 'ny':
-        ny = i.text
-    elif i.tag == 'obsrValue':
-        obsrValue = i.text
-
-        rows.append({"baseDate": baseDate,
-                 "baseTime": baseTime,
-                 "category": category,
-                 "nx": nx,
-                 "ny": ny,
-                 'obsrValue': obsrValue})
-
-df = pd.DataFrame(rows, columns=cols)
-
-# Writing dataframe to csv
-df.to_csv('output.csv')
-"""
 
